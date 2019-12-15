4083603e36be04de00b07215f1224a1edee7ae39
Babushkin Roman <bagumices@gmail.com>
2019-12-15 19:06:01 +0500

    solve BeautifulStrings problem

--- BeautifulStrings.hs
+++ BeautifulStrings.hs
@@ -1,62 +1,65 @@
-import Control.Monad
+-- https://codeforces.com/contest/1265/problem/A
+-- 3
+-- a???cb => ababcb
+-- a??bbc => -1
+-- a?b?c => acbac
+
 
 main :: IO ()
 main = do
-  quantity <- getLine
-  let linesToRead = read quantity
-  testCases <- replicateM linesToRead getLine
-  mapM_ (putStrLn .  tryDecorateString) testCases
-
-tryDecorateString :: [Char] -> [Char]
-tryDecorateString input = 
-  if isStringBeautiful input Nothing
-  then decorateString Nothing input
-  else "-1"
-
+  input <- getLine
+  let linesToRead = read input
+  tryDecorateString linesToRead
+  
+tryDecorateString :: Int -> IO ()
+tryDecorateString 0 = pure ()
+tryDecorateString n = do
+  currentLine <- getLine
+  if isStringBeautiful currentLine Nothing
+  then putStrLn (reverse (decorateString currentLine []))
+  else putStrLn "-1"
+  tryDecorateString (n - 1)
+  
 alphabet :: [Char]
 alphabet = ['a', 'b', 'c']
-
+  
 -- check that string doesn't contains any double characters
 isStringBeautiful :: [Char] -> Maybe Char -> Bool
 isStringBeautiful (x:xs) Nothing =
   if (length xs) == 0
   then True
   else isStringBeautiful xs (Just x)
-
+  
 isStringBeautiful (x:xs) (Just prevChar) =
   if x == prevChar && x /= '?'
   then False
   else isStringBeautiful xs (Just x)
-
+  
 isStringBeautiful [] _ = True
 
--- beautify string
-decorateString :: Maybe [Char] -> [Char] -> [Char]
-decorateString Nothing (currentChar:otherChars)
-  | length otherChars == 0 && currentChar == '?' = [anyChar]
-  | length otherChars == 0 && currentChar /= '?' = [currentChar]
-  | length otherChars > 0  && currentChar == '?' = decorateString (Just [allowedChar]) otherChars 
-  | length otherChars > 0  && currentChar /= '?' = decorateString (Just [currentChar]) otherChars 
+decorateString :: [Char] -> [Char] -> [Char]
+decorateString word formattedWord =
+  case word of
+  (x:[]) -> case x of
+    '?' -> case formattedWord of
+      -- start iteration
+      [] -> [anyChar] 
+      -- end of iteration
+      (r:rs) -> allowedEnd:r:rs
+    _ -> x:formattedWord
+  (x:xs) -> case x of
+    '?' -> case formattedWord of
+      [] -> decorateString xs (allowedStart:[])
+      (r:rs) -> decorateString xs (allowedMiddle:r:rs)
+    _   -> decorateString xs (x:formattedWord)
   where
     anyChar = alphabet !! 0
-    nextChar = head otherChars
-    allowedChar = (filter (/= nextChar) alphabet) !! 0
-
-decorateString (Just formatedString) (currentChar:[])
-  | currentChar == '?' = formatedString ++ [allowedChar]
-  | otherwise = formatedString ++ [currentChar]
-  where
-    previousChar = last formatedString
-    allowedChar = (filter (/= previousChar) alphabet) !! 0
-
-decorateString (Just formatedString) (currentChar:otherChars)
-  | currentChar == '?' = decorateString (Just (formatedString ++ [allowedChar])) otherChars
-  | currentChar /= '?' = decorateString (Just (formatedString ++ [currentChar])) otherChars
-  where
-    nextChar = head otherChars
-    previousChar = last formatedString
-    allowedChar = (filter (\char -> char /= previousChar && char /= nextChar) alphabet) !! 0
-
-
+    nextChar = word !! 1
+    prevChar = head formattedWord
+    allowedStart = (filter (/= nextChar) alphabet) !! 0
+    allowedMiddle = (filter (\x -> x /= prevChar && x /= nextChar) alphabet) !! 0
+    allowedEnd = (filter (/= prevChar) alphabet) !! 0
+ 
+  
 tests :: [String]
 tests = ["a???cb", "a??bbc", "a?b?c", "a??a"]
